ba4f609882bf6caeb29709fd832e23a3629dc856
MurithiDenisGitobu <iamdenis46@gmail.com>
2024-06-05 22:34:02 +0300

    Some improvements done on the data page

--- pages/01_📈Data.py
+++ pages/01_📈Data.py
@@ -6,40 +6,46 @@ import pandas as pd
 # Configure the home page
 st.set_page_config(
     page_title="CUSTOMER CHURN PREDICTION APPLICATION",
-    page_icon='🏠',
+    page_icon='📈',
     layout="wide"
 )
 
+@st.cache_resource
+def get_db_connection():
+    connection_string = (
+        "DRIVER={SQL Server};"
+        f"SERVER={st.secrets['SERVER']};"
+        f"DATABASE={st.secrets['DATABASE']};"
+        f"UID={st.secrets['USERNAME']};"
+        f"PWD={st.secrets['PASSWORD']}"
+    )
+    return pyodbc.connect(connection_string)
+@st.cache_data
+def get_sql_data(_connection):
+    churn_data_query = "SELECT * FROM dbo.LP2_Telco_churn_first_3000"
+    return pd.read_sql_query(churn_data_query, _connection)
+
+
+@st.cache_data
+def get_github_data():
+    churn_data_csv = "Data/LP2_Telco-churn-second-2000.csv"
+    return pd.read_csv(churn_data_csv)
+
 # Create tabs with "All Data" as the default selected tab
 tab_names = ["All Data", "Data from SQL Server", "Data from GitHub Repository"]
-tab_idx = 0  # Index for the default tab (0 for "All Data")
-
-# Tab creation and selection handling
 tabs = st.tabs(tab_names)
+
+# Fetch data
+connection = get_db_connection()
+churn_data_sql = get_sql_data(connection)
+churn_data_github = get_github_data()
 
 # Tab 1: All Data
 with tabs[0]:
     st.header("All Data")
     st.subheader("Data from SQL Server and GitHub Repository")
 
-    with st.spinner("Connecting to Database and Loading Data...."):
-        # SQL Server data
-        connection_string = (
-            "DRIVER={SQL Server};"
-            f"SERVER={st.secrets['SERVER']};"
-            f"DATABASE={st.secrets['DATABASE']};"
-            f"UID={st.secrets['USERNAME']};"
-            f"PWD={st.secrets['PASSWORD']}"
-        )
-        connection = pyodbc.connect(connection_string)
-        churn_data_query = "SELECT * FROM dbo.LP2_Telco_churn_first_3000"
-        churn_data_sql = pd.read_sql_query(churn_data_query, connection)
-
-        # GitHub Repository data
-        churn_data_csv = "Data/LP2_Telco-churn-second-2000.csv"
-        churn_data_github = pd.read_csv(churn_data_csv)
-
-        # Concatenate the data
+    with st.spinner("Concatenating all data...."):
         combined_data = pd.concat([churn_data_sql, churn_data_github], ignore_index=True)
     
     # Display the concatenated data
@@ -48,17 +54,11 @@ with tabs[0]:
 # Tab 2: Data from SQL Server
 with tabs[1]:
     st.header("Data from SQL Server")
-    with st.spinner("Connecting to Database...."):
-        churn_data_sql = pd.read_sql_query(churn_data_query, connection)
-    
     # Display the data
     st.write(churn_data_sql)
 
 # Tab 3: Data from GitHub Repository
 with tabs[2]:
     st.header("Data from GitHub Repository")
-    with st.spinner("Loading data from GitHub Repository...."):
-        churn_data_github = pd.read_csv(churn_data_csv)
-    
     # Display the data
     st.write(churn_data_github)
